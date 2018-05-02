f30b435d0b12e17b7dcea7409301ae16f6f7819a
jpvandy <jpvandy@sandia.gov>
2018-05-02 17:25:56 -0600

    correct module syntax

--- test/testSuites/testSuite_scheduler.sh
+++ test/testSuites/testSuite_scheduler.sh
@@ -423,9 +423,9 @@ test_scheduler_0005() {
     fi
 
 ##########################################################
-    module avail metis
+    module avail 
     echo   "### module in use +++"
-    module list metis
+    module list 
     echo ' '
 
     outFile="${SST_TEST_OUTPUTS}/${testDataFileBase}.out"
@@ -523,9 +523,9 @@ test_scheduler_0005a() {
     fi
 
 ##########################################################
-    module avail metis
+    module avail 
     echo   "### module in use +++"
-    module list metis
+    module list 
     module rm metis
     module load metis
     module list metis
